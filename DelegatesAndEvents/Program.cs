8a770e0b8667d3b95d6b35667ceb16fab516c0a3
Juan Jose Herrera Espinoza <juan.herrerae@gmail.com>
2016-07-24 20:25:24 -0500

    Add first delagates and calls

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkPerformedHandler del1 = 
+                new WorkPerformedHandler(WorkPerformed1);
+
+            WorkPerformedHandler del2 =
+                new WorkPerformedHandler(WorkPerformed1);
+
+            //del1(5, WorkType.Golf);
+            //del2(10, WorkType.GenerateReports);
+
+            DoWork(del2);
+            
+        }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.GoToMeetings);
+        }
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed1 call " + hours.ToString());
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed2 call " + hours.ToString());
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
